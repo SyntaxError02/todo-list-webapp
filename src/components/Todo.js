53b00d49705d7723dab6bb09551d641d51776901
Kolade Alabi <koladealabi@Kolades-MBP.lan>
2022-01-04 12:19:48 -0600

    completed the todo list task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
+import { TiEdit, TiInputChecked } from 'react-icons/ti';
 import { RiCloseCircleLine } from 'react-icons/ri';
-import { TiEdit } from 'react-icons/ti'
 import TodoForm from './TodoForm';
 
 const Todo = ({todos, completeTodo, removeTodo, updateTodo}) => {
@@ -18,25 +18,52 @@ const Todo = ({todos, completeTodo, removeTodo, updateTodo}) => {
         });
     };
 
+    function CompletedTodos() {
+        return (
+            todos.map((todo, index) => (
+                todo.isComplete ?
+            <div className="todoItem" key ={ index}  >
+                <div>
+                <TiInputChecked onClick={() => completeTodo(todo.id)}/> {todo.text}
+                </div>
+                <div className="btns"> <RiCloseCircleLine onClick={() => removeTodo(todo.id)}/> </div>
+            </div>
+            : <div></div>))
+        )
+    }
+
+    function Todos(){
+        return(
+            todos.map((todo, index) => 
+             ( !todo.isComplete ?
+           <div className="todoItem" key={index}>
+          
+                <div>  <TiInputChecked onClick={() => completeTodo(todo.id)}/> {todo.text}</div>
+              
+            <div className="btns"> <TiEdit onClick={() => setEdit({id: todo.id, value: todo.text })}/> <RiCloseCircleLine onClick={() => removeTodo(todo.id)}/> </div>
+            
+                   
+                </div> : '')
+        )
+        )
+    }
+
     if(edit.id) {
         return <TodoForm edit={edit} onSubmit={submitUpdate} />;
     }
 
-    return todos.map((todo, index) => (
-        <div className={todo.isComplete? 'item w-75 d-flex flex-row justify-content-between complete' : 'item w-75 d-flex flex-row justify-content-between'} key={index}>
-            <div key={index} onClick={() => completeTodo(todo.id)}>{todo.text}</div>
-            <div className='icons'>
-                <span className="delete btn btn-info d-inline-flex justify-content-center align-content-center" onClick={() => setEdit({id: todo.id, value: todo.text })}>
-                    <span>Edit</span>&nbsp;
-                    <span><RiCloseCircleLine /></span>    
-                </span>&nbsp;&nbsp;
-                <span className="edit btn btn-danger d-inline-flex justify-content-center align-content-center" onClick={() => removeTodo(todo.id)}>
-                    <span>Delete</span>&nbsp;
-                    <span><TiEdit /></span>
-                </span>
+    return (
+        <div>
+            <div>
+                <div className="header">Task</div> 
+      
             </div>
+            <Todos/>
+            <div className="header">Completed</div> 
+            <CompletedTodos/>
         </div>
-    ))
+       
+    )
 }
 
 export default Todo;
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -31,14 +31,11 @@ const TodoForm = (props) => {
     return (
         <div className="row">
             {
-                props.edit?
-                (<form className="col-12 d-flex flex-row text-center" onSubmit={handleSubmit}>
-                <input type="text" placeholder="Update a todo" onChange={handleChange} value={input} ref={inputRef} className="form-control w-50" />&nbsp;&nbsp;
-                <button className="btn btn-primary col-2">Update</button>
-                {/* &nbsp;&nbsp;<span className="btn btn-danger col-2" onClick={handleCancel}>Cancel</span> */}
-            </form>) : (<form className="col-12 d-flex flex-row text-center" onSubmit={handleSubmit}>
-                <input type="text" placeholder="Add a todo" onChange={handleChange} ref={inputRef} value={input} className="form-control w-50" />&nbsp;&nbsp;
-                <button className="btn btn-primary col-2">Save</button>
+                props.edit ==="undefined"?
+                (<form className="todoForm" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Update a todo" onChange={handleChange} value={input} ref={inputRef} className="inputForm" />&nbsp;&nbsp;
+            </form>) : (<form className="todoForm" onSubmit={handleSubmit}>
+                <input type="text" placeholder="Add a todo" onChange={handleChange} ref={inputRef} value={input} className="inputForm" />&nbsp;&nbsp;
             </form>)
             }
         </div>
